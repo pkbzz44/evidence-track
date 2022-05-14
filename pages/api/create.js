06b04f1504c06b338b24d43074074eb4f9e22815
Paekabu <pae9769@gmail.com>
2022-05-14 12:09:34 +0700

    fix create api

--- pages/api/create.js
+++ pages/api/create.js
@@ -3,23 +3,19 @@ import bcrypt from 'bcrypt';
 const saltRounds = 10;
 
 export default function handler(req, res) {
+  const { name, password } = req?.body;
   const prisma = new PrismaClient();
 
   async function main() {
-    // Connect the client
     await prisma.$connect();
-    let password = '0899999999';
-    bcrypt.hash(password, saltRounds, async (err, hash) => {
-      const user = await prisma.user.create({
-        data: {
-          name: 'kwandaoh',
-          password: hash,
-        },
-      });
-      res.status(200).json(user);
+    const hashedPassword = await bcrypt.hash(password, saltRounds);
+    const user = await prisma.user.create({
+      data: {
+        name,
+        password: hashedPassword,
+      },
     });
-
-    // ... you will write your Prisma Client queries here
+    res.status(200).json(user);
   }
 
   main()
--- pages/api/login.js
+++ pages/api/login.js
@@ -8,7 +8,6 @@ export default function handler(req, res) {
   const prisma = new PrismaClient();
 
   async function main() {
-    // Connect the client
     await prisma.$connect();
     const user = await prisma.user.findUnique({
       where: {
@@ -22,16 +21,7 @@ export default function handler(req, res) {
       const token = jwt.sign(user, process.env.JWT_KEY);
       return res.status(200).json(token);
     }
-    // const password = await bcrypt.hash('0899999999', saltRounds);
-    // const user = await prisma.user.create({
-    //   data: {
-    //     name: 'kwandaoh',
-    //     password,
-    //   },
-    // });
     return res.status(401).json('Unauthorized');
-
-    // ... you will write your Prisma Client queries here
   }
 
   main()
