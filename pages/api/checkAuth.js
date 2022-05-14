e600a0cf549e2e2f52d564ae0bc3ec2e9d52a452
Paekabu <pae9769@gmail.com>
2022-05-14 12:12:26 +0700

    fix checkAuth api

--- pages/api/checkAuth.js
+++ pages/api/checkAuth.js
@@ -1,27 +1,13 @@
-import { PrismaClient } from '@prisma/client';
-import bcrypt from 'bcrypt';
 import jwt from 'jsonwebtoken';
 
 export default function handler(req, res) {
-  const prisma = new PrismaClient();
-  const token = req.headers.Authorization;
+  const authorizationHeader = req.headers.authorization;
+  const token = authorizationHeader.split('Bearer ')[1];
 
-  async function main() {
-    // Connect the client
-    try {
-      const verify = jwt.verify(token, process.env.JWT_KEY);
-      res.status(200).json(verify);
-    } catch (error) {
-      res.status(401).json(error);
-    }
-    // ... you will write your Prisma Client queries here
+  try {
+    const verify = jwt.verify(token, process.env.JWT_KEY);
+    return res.status(200).json(verify);
+  } catch (error) {
+    return res.status(401).json(error);
   }
-
-  main()
-    .catch((e) => {
-      throw e;
-    })
-    .finally(async () => {
-      await prisma.$disconnect();
-    });
 }
