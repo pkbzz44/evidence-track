147aed2a8d6ba766070b0bcbfc33b8c4108a6c5a
Paekabu <pae9769@gmail.com>
2022-05-14 12:21:40 +0700

    Add axios

--- pages/login.js
+++ pages/login.js
@@ -7,17 +7,29 @@ import {
   VStack,
 } from '@chakra-ui/react';
 import React from 'react';
+import { useForm } from 'react-hook-form';
 
 const Login = () => {
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm();
+  const onSubmit = (data) => console.log(data);
   return (
     <Container maxW='container.md' mt='24'>
       <Center>
         <Heading mb='4'>เข้าสู่ระบบ</Heading>
       </Center>
-      <form>
+      <form onSubmit={handleSubmit(onSubmit)}>
         <VStack>
-          <Input placeholder='กรุณากรอกชื่อ' />
-          <Input placeholder='กรุณากรอกรหัสผ่าน' />
+          <Input placeholder='กรุณากรอกชื่อ' {...register('name')} />
+          <Input
+            type='password'
+            placeholder='กรุณากรอกรหัสผ่าน'
+            {...register('password')}
+          />
           <Button colorScheme='blue' type='submit'>
             ยืนยัน
           </Button>
