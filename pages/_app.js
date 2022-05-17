a37dfec71742fb75a12eb563e2705a53bb881439
Paekabu <pae9769@gmail.com>
2022-05-18 00:51:29 +0700

    update create new ev page

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,7 @@
 import '../styles/globals.css';
 import { ChakraProvider } from '@chakra-ui/react';
+import 'react-date-picker/dist/DatePicker.css';
+import 'react-calendar/dist/Calendar.css';
 function MyApp({ Component, pageProps }) {
   return (
     <ChakraProvider>
--- pages/index.js
+++ pages/index.js
@@ -9,11 +9,15 @@ import {
   Td,
   TableCaption,
   TableContainer,
+  Heading,
+  Flex,
+  Button,
 } from '@chakra-ui/react';
 import { useRouter } from 'next/router';
 import React, { useEffect, useState } from 'react';
 import AxiosInstance from '../lib/api';
 import dayjs from 'dayjs';
+import Link from 'next/link';
 
 if (typeof window !== 'undefined') {
   AxiosInstance.defaults.headers.common = {
@@ -46,75 +50,80 @@ const Home = () => {
   }, []);
 
   return (
-    <div>
-      <h1>รายการหลักฐาน</h1>
-      {/* <TableContainer> */}
-      <Table variant='simple'>
-        <Thead>
-          <Tr>
-            <Th>ก</Th>
-            <Th>วันที่รับของ</Th>
-            <Th>สน</Th>
-            <Th>เลขหนังสือนำส่ง</Th>
-            <Th>ลงวันที่</Th>
-            <Th>พงส</Th>
-            <Th>โทร พงส</Th>
-            <Th>รายการของกลาง</Th>
-            <Th>ตู้</Th>
-            <Th>เทคนิค</Th>
-            <Th>ร้อยเวร</Th>
-            <Th>ผู้ช่วย</Th>
-            <Th>สถานะ</Th>
-          </Tr>
-        </Thead>
-        <Tbody>
-          {evidences.map(
-            ({
-              id,
-              evidenceId,
-              receivedDate,
-              policeStation,
-              packageId,
-              packageDate,
-              detectiveName,
-              detectivePhone,
-              itemsDescription,
-              storedAt,
-              LTName,
-              technique,
-              status,
-              owner,
-            }) => (
-              <>
-                <Tr key={id}>
-                  <Td>{evidenceId}</Td>
-                  <Td>{dayjs(receivedDate).format('DD/MM/YY')}</Td>
-                  <Td>{policeStation}</Td>
-                  <Td>{packageId}</Td>
-                  <Td>{dayjs(packageDate).format('DD/MM/YY')}</Td>
-                  <Td>{detectiveName}</Td>
-                  <Td>{detectivePhone}</Td>
-                  <Td>{itemsDescription}</Td>
-                  <Td>{storedAt}</Td>
-                  <Td>{technique}</Td>
-                  <Td>{LTName}</Td>
-                  <Td>{owner.name}</Td>
-                  <Td>{status}</Td>
-                </Tr>
-              </>
-            )
-          )}
-        </Tbody>
-        {/* <Tfoot>
+    <Container maxW={'container.lg'} mt='8'>
+      <Heading mb='4'>รายการหลักฐาน</Heading>
+      <Flex justify={'end'}>
+        <Link href='/new'>
+          <Button colorScheme={'blue'}>สร้างรายการ</Button>
+        </Link>
+      </Flex>
+      <TableContainer>
+        <Table variant='striped' colorScheme={'teal'}>
+          <Thead>
+            <Tr>
+              <Th>สถานะ</Th>
+              <Th>ก</Th>
+              <Th>วันที่รับของ</Th>
+              <Th>สน</Th>
+              <Th>เลขหนังสือนำส่ง</Th>
+              <Th>ลงวันที่</Th>
+              <Th>พงส</Th>
+              <Th>โทร พงส</Th>
+              <Th>รายการของกลาง</Th>
+              <Th>ตู้</Th>
+              <Th>เทคนิค</Th>
+              <Th>ร้อยเวร</Th>
+              <Th>ผู้ช่วย</Th>
+            </Tr>
+          </Thead>
+          <Tbody>
+            {evidences.map(
+              ({
+                id,
+                evidenceId,
+                receivedDate,
+                policeStation,
+                packageId,
+                packageDate,
+                detectiveName,
+                detectivePhone,
+                itemsDescription,
+                storedAt,
+                LTName,
+                technique,
+                status,
+                owner,
+              }) => (
+                <>
+                  <Tr key={id}>
+                    <Td>{status}</Td>
+                    <Td>{evidenceId}</Td>
+                    <Td>{dayjs(receivedDate).format('DD/MM/YY')}</Td>
+                    <Td>{policeStation}</Td>
+                    <Td>{packageId}</Td>
+                    <Td>{dayjs(packageDate).format('DD/MM/YY')}</Td>
+                    <Td>{detectiveName}</Td>
+                    <Td>{detectivePhone}</Td>
+                    <Td>{itemsDescription}</Td>
+                    <Td>{storedAt}</Td>
+                    <Td>{technique}</Td>
+                    <Td>{LTName}</Td>
+                    <Td>{owner.name}</Td>
+                  </Tr>
+                </>
+              )
+            )}
+          </Tbody>
+          {/* <Tfoot>
             <Tr>
               <Th>To convert</Th>
               <Th>into</Th>
               <Th isNumeric>multiply by</Th>
             </Tr>
           </Tfoot> */}
-      </Table>
-      {/* </TableContainer> */}
-    </div>
+        </Table>
+      </TableContainer>
+    </Container>
   );
 };
 
