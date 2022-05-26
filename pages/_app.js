24ac7cca6934acd686c0aa3f030837c5161f2aee
Vachirarat Thavisaengsiri <vachiraratt@arv.co.th>
2022-05-26 20:16:22 +0700

    Feat: Add layout and navbar

--- pages/_app.js
+++ pages/_app.js
@@ -2,10 +2,13 @@ import '../styles/globals.css';
 import { ChakraProvider } from '@chakra-ui/react';
 import 'react-date-picker/dist/DatePicker.css';
 import 'react-calendar/dist/Calendar.css';
+import Layout from '../components/layout';
 function MyApp({ Component, pageProps }) {
   return (
     <ChakraProvider>
-      <Component {...pageProps} />
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
     </ChakraProvider>
   );
 }
